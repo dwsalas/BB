f164de17682c314d97fb0fc9cef7258a72f65906
dondapogi <122374918+dwsalas@users.noreply.github.com>
2024-01-30 19:11:09 -0500

    ADDED I LOVE YOU

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ function showMessage(response) {
   
       // Update text content, show message, and change image source to "dance.gif"
       const yesMessage = document.getElementById("question");
-      yesMessage.textContent = "LESGOOO see you on the 14th babyy😘😘";
+      yesMessage.textContent = "LESGOOO see you on the 14th babyy 😘😘"; 
+      yesMessage.textContent = "❤️I LOVE YOU❤️";  
       yesMessage.style.display = "block";
       yesMessage.style.fontStyle = "normal";
       document.getElementsByClassName("image")[0].src = "images/dance.gif";
