754a9a4860afa7cb8e621aa75eb7d5929b184155
dondapogi <122374918+dwsalas@users.noreply.github.com>
2024-01-30 19:35:46 -0500

    TEXT

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
   
       // Update text content, show message, and change image source to "dance.gif"
       const yesMessage = document.getElementById("question");
-      yesMessage.textContent = "LESGOOO see you on the 14th babyy 😘😘" + "I LOVE YOU ❤️";
+      yesMessage.innerHTML = "LESGOOO see you on the 14th babyy 😘😘<br>I LOVE YOU ❤️";
       yesMessage.style.display = "block";
       yesMessage.style.fontStyle = "normal";
       document.getElementsByClassName("image")[0].src = "images/dance.gif";
